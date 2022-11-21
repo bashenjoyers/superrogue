1390bf9f4dde16d4c84224df1119d7a69ce67581
mandelshtamd <mandelstudy@gmail.com>
2022-11-21 19:22:53 +0300

    fix syntax errors

--- src/Model/GameModel/Map/Generator/FileGenerating/FileGenerator.cpp
+++ src/Model/GameModel/Map/Generator/FileGenerating/FileGenerator.cpp
@@ -1,14 +1,14 @@
 #include "FileGenerator.h"
 
 GameModel::Map::FileGenerator::FileGenerator() {
-    fileReader = std::make_shared<FileReader>();
-    mapEntityProvider = std::make_shared<MapEntityProvider>();
-    environmentVariablesProvider = std::make_shared<Common::EnvironmentVariablesProvider>();
+    fileReader = FileReader();
+    mapEntityProvider = MapEntityProvider();
+    environmentVariablesProvider = Common::EnvironmentVariablesProvider();
 }
 
 std::vector<std::vector<GameModel::Abstract::MapEntity>> GameModel::Map::FileGenerator::generate() {
-    std::string pathToMap = environmentVariablesProvider->getEnvironmentVariable(mapEnvVariableKey);
-    std::vector<std::string> fileContent = fileReader->readFile(pathToMap);
+    std::string pathToMap = environmentVariablesProvider.getEnvironmentVariable(mapEnvVariableKey);
+    std::vector<std::string> fileContent = fileReader.readFile(pathToMap);
     return convertFileContentToMap(fileContent);
 }
 
@@ -20,7 +20,7 @@ GameModel::Map::FileGenerator::convertFileContentToMap(const std::vector<std::st
         std::vector<Abstract::MapEntity> mapLine = std::vector<Abstract::MapEntity>();
 
         for (char fileLineChar: fileLine) {
-            Abstract::MapEntity mapEntity = mapEntityProvider->getMapEntityBySymbol(fileLineChar);
+            Abstract::MapEntity mapEntity = mapEntityProvider.getMapEntityBySymbol(fileLineChar);
             mapLine.push_back(mapEntity);
         }
 
