963fed99f35ca083f5d659755616f9e557a575ea
mandelshtamd <mandelstudy@gmail.com>
2022-11-21 22:54:50 +0300

    add comments for controller module

--- src/Controller/EventProvider.h
+++ src/Controller/EventProvider.h
@@ -11,13 +11,38 @@
 
 namespace Controller {
 
+/**
+ * @brief a class that provides event by key
+ */
 class EventProvider {
 public:
+  /**
+  * @brief constructs a new Event Provider object
+  *
+  * @param cursorState - represents state of user's cursor
+  * @param map - represents game map
+  */
   EventProvider(std::shared_ptr<UIModel::CursorState> cursorState,
                 std::shared_ptr<GameModel::Map::Map> map);
+  /**
+  * @brief provides event by user's key
+  *
+  * @param key - represents user's key
+  * @return shared_ptr<IEvent> - event for user's key
+  */
   std::shared_ptr<GameModel::Events::IEvent> getEventByKey(int key);
 
+  /**
+  * @brief adds observer for UI events
+  *
+  * @param obs - represents observer for UI events
+  */
   void addUIObserver(std::shared_ptr<UIModelObserver> obs);
+  /**
+  * @brief adds observer for Model events
+  *
+  * @param obs - represents observer for Model events
+  */
   void addModelObserver(std::shared_ptr<GameModelObserver> obs);
 
 private:
--- src/Controller/GameController.h
+++ src/Controller/GameController.h
@@ -6,12 +6,36 @@
 
 namespace Controller {
 
+/**
+ * @brief a class that interacts with user input data
+ */
 class GameController {
 public:
+
+  /**
+  * @brief constructs a new Game Controller object
+  *
+  * @param provider - provides event to execute after user's key pressed
+  */
   GameController(std::unique_ptr<EventProvider> provider);
+
+  /**
+  * @brief method to handle a single user's key
+  */
   void interact();
 
+  /**
+  * @brief adds observer for UI events
+  *
+  * @param obs - observer to add
+  */
   void addUIObserver(std::shared_ptr<UIModelObserver> obs);
+
+  /**
+  * @brief adds observer for Model events
+  *
+  * @param obs - observer to add
+  */
   void addModelObserver(std::shared_ptr<GameModelObserver> obs);
 
 private:
