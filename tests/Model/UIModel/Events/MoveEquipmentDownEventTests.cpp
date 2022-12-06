a279c5fe2f8cd5e37b407c5dbcc27eaeb21f298f
andrew <p-a-k-12@mail.ru>
2022-12-06 21:59:15 +0300

    fix tests

--- tests/Model/UIModel/Events/MoveEquipmentDownEventTests.cpp
+++ tests/Model/UIModel/Events/MoveEquipmentDownEventTests.cpp
@@ -1,20 +1,28 @@
 #include <gtest/gtest.h>
 #include "Model/GameModel/Map/Map.h"
 #include "Model/UIModel/Events/MoveEquipmentDownEvent.h"
+#include "Model/GameModel/GameObject/Character/Generation/AbstractEnemyFactory.h"
+#include "Model/GameModel/GameObject/Character/Generation/FantasyEnemyFactory.h"
+#include "Model/GameModel/Map/Generator/RandomGenerating/BinaryTreeMazeGenerator.h"
 #include <memory>
 
 
-// TEST(MOVE_EQUIPMENT_DOWN_EVENT_TESTS, TEST_EXECUTE) {
-//     std::set<std::shared_ptr<GameModel::IEnemy>> enemies = std::set<std::shared_ptr<GameModel::IEnemy>>();
-//     std::shared_ptr<GameModel::Person> person = std::make_shared<GameModel::Person>();
-//     GameModel::Map::MapOptions mapOptions = GameModel::Map::MapOptions();
-//     mapOptions.height = 24;
-//     mapOptions.width = 24;
-//     std::shared_ptr<GameModel::Map::Map> map = std::make_shared<GameModel::Map::Map>(enemies, person, mapOptions, 0);
+TEST(MOVE_EQUIPMENT_DOWN_EVENT_TESTS, TEST_EXECUTE) {
+    std::shared_ptr<GameModel::Person> person = std::make_shared<GameModel::Person>();
+    GameModel::Map::MapOptions mapOptions = GameModel::Map::MapOptions {
+        .height = 24,
+        .width = 24
+    };
+    int level = 1;
 
-//     std::shared_ptr<UIModel::CursorState> cursorState = std::make_shared<UIModel::CursorState>();
-//     GameModel::Events::MoveEquipmentDownEvent moveEquipmentDownEvent = GameModel::Events::MoveEquipmentDownEvent(map, cursorState);
+    std::shared_ptr<GameModel::Generation::ItemGenerator> itemGenerator = std::make_shared<GameModel::Generation::ItemGenerator>(level, person->get_full_characteristics().luck);
+    std::shared_ptr<GameModel::Generation::Map::MapGenerator> mapGenerator = std::make_shared<GameModel::Generation::Map::BinaryTreeMazeGenerator>();
+    std::shared_ptr<GameModel::Generation::AbstractEnemyFactory> enemyFactory = std::make_shared<GameModel::Generation::FantasyEnemyFactory>(level);
+    std::shared_ptr<GameModel::Map::Map> map = std::make_shared<GameModel::Map::Map>(itemGenerator, mapGenerator, enemyFactory, person, mapOptions, 0);
 
-//     moveEquipmentDownEvent.execute();
-//     ASSERT_EQ(cursorState->getEquipmentCursor(), GameModel::ItemType::WEAPON_DISTANT);
-// }
+    std::shared_ptr<UIModel::CursorState> cursorState = std::make_shared<UIModel::CursorState>();
+    GameModel::Events::MoveEquipmentDownEvent moveEquipmentDownEvent = GameModel::Events::MoveEquipmentDownEvent(map, cursorState);
+
+    moveEquipmentDownEvent.execute();
+    ASSERT_EQ(cursorState->getEquipmentCursor(), GameModel::ItemType::WEAPON_DISTANT);
+}
